70fb3f3ef36d90c97e864b67e81ed9a59f81281f
Tiger <billath131908@gmail.com>
2025-01-13 05:55:43 +0200

    c

--- cuda1.cu
+++ cuda1.cu
@@ -378,7 +378,7 @@ int main(int argc, char *argv[])
     {
         arr = 'C';
 
-        createC<<<nBlocks, nThreads>>>(d_A, d_OutArr);
+        createC<<<dimGrid, dimBlock>>>(d_A, d_OutArr);
 
         err = cudaMemcpy(h_OutArr, d_OutArr, floatBytes, cudaMemcpyDeviceToHost);
         if (err != cudaSuccess) { printf("CUDA Error --> cudaMemcpy(&h_OutArr, d_OutArr, doubleBytes, cudaMemcpyDeviceToHost) failed."); exit(1); }
